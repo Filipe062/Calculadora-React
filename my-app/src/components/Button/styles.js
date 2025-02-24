244d08dc53df5dd298c5239f8bcc664616d50a58
Filipe062 <luisfhylipe@gmail.com>
2025-02-24 15:54:02 -0300

    terciro

--- my-app/src/components/Button/styles.js
+++ my-app/src/components/Button/styles.js
@@ -1,1 +1,12 @@
 import styled from 'styled-components';
+
+
+export const ButtonContainer = styled.button`
+     padding: 10px; /* Fixed the typo here */
+     border: 1px solid #CDCDCD;
+     background-color: #00aaf0;
+     color: #ffffff;
+     font-size: 24px;
+     font-weight: 700;
+     flex: 1;
+`;
--- my-app/src/styles.js
+++ my-app/src/styles.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   width: 100%;
-  height: 100vh;  /* Corrected the typo here */
+  height: 100vh; /* Corrected the typo here */
   background-color: rgb(247, 181, 225);
 
   display: flex;
@@ -10,9 +10,22 @@ export const Container = styled.div`
   justify-content: center;
 `;
 
-
 export const Content = styled.div`
-   background-color:rgb(161, 101, 153);
-   width: 80%;
-   min-height: 350px;
-`
+   background-color: rgb(161, 101, 153);
+   width: 50%; /* Added missing semicolon */
+   
+`;
+
+export const Row = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const Column = styled.div`
+  display: flex;
+  flex-direction: column; /* Fixed the case here */
+  justify-content: space-between;
+  align-items: center;
+`;
